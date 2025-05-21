8627b9bc2d52a3e56bb8a0475e9c61632688f9b2
ConchaCF <ccotrimferreira@gmail.com>
2025-05-21 18:46:59 +0100

    Esqueleto

--- genetic.py
+++ genetic.py
@@ -7,9 +7,22 @@ def generate_population(individual_size, population_size):
     return [create_individual(individual_size) for _ in range(population_size)]
 
 def genetic_algorithm(individual_size, population_size, fitness_function, target_fitness, generations, elite_rate=0.2, mutation_rate=0.05):
-    population = generate_population(individual_size, population_size)
+    population = generate_population(individual_size, population_size) #population initial
     best_individual = None
+   
+    #while target fitness != bestIndividual fitness e generations != generations 
+
+    #avaliar individuals com a fittness function dada
+    #vai atualizando o best_individual
+
+    #selecionar os melhor 0.2 individos para guardar na nova populacao (elitism)
+
+    #ate ter preenchido a nova populacao
+    #escolher pais (tournament)
+    #crossover (one point)
+    #mutation (scramble)
     
-    # Complete the algorithm
+    #population= newPop
+    #endwhile
 
     return best_individual # This is expected to be a pair (individual, fitness)
