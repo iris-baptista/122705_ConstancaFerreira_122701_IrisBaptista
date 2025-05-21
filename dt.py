39365b0a4dc39bdaffee5f08b1d33c2f56abbe0f
iris <81236799+iris-baptista@users.noreply.github.com>
2025-05-21 16:40:35 +0100

    Atualização files v1.1

---
+++ dt.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class DecisionTree:
+
+    def __init__(self, X, y, threshold=1.0, max_depth=None): # Additional optional arguments can be added, but the default value needs to be provided
+        # Implement this
+        pass
+
+    def predict(self, x): # (e.g. x = ['apple', 'green', 'circle'] -> 1 or -1)
+        # Implement this
+        pass
+
+
+def train_decision_tree(X, y):
+    # Replace with your configuration
+    return DecisionTree(X, y)
